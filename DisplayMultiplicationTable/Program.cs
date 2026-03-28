306793b4212a21c540a8b0e5a866522d9269738c
Javier Flores <jflores0089@gmail.com>
2026-03-28 13:08:27 -0400

    Add DisplayMultiplicationTable program

---
+++ DisplayMultiplicationTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+class DisplayMultiplicationTables
+{
+    static void Main()
+    {
+       for(int x = 1; x <= 10; ++x)
+        {
+        for(int y = 1; y <= 10; ++y)
+            {
+                WriteLine("{0}*{1} = {2}",x,y,(x*y));
+            }  
+        }
+
+
+    }
+}
