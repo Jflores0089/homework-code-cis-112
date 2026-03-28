1772f00972003e6f8882e76fe472094b9c297c30
Javier Flores <jflores0089@gmail.com>
2026-03-28 17:44:18 -0400

    Add CountVowels Program

---
+++ CountVowels/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+class CountVowels
+{
+    static void Main()
+    {
+    WriteLine("Please enter a word");
+    string? userInput = ReadLine();
+    if (string.IsNullOrWhiteSpace(userInput))
+{
+    userInput = "there";
+}
+    int counter = 0;
+
+        foreach (char currentChar in userInput)
+        {
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u' || currentChar == 'A' || currentChar == 'E' || currentChar == 'I' || currentChar == 'O' || currentChar == 'U')
+            {
+                ++counter;
+            } 
+        }
+        WriteLine("The Vowel Count is {0}.", counter);
+    }
+}
