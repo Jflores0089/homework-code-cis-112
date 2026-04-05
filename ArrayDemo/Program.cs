ca409de33912e649af7c041541b887045f841922
Javier Flores <jflores0089@gmail.com>
2026-04-05 18:29:03 -0400

    updating array code

--- ArrayDemo/Program.cs
+++ ArrayDemo/Program.cs
@@ -1,5 +1,28 @@
-﻿
-namespace ArrayDemo
+﻿using static System.Console;
+
+class ArrayDemo
 {
-  using System.Console;  
+  static void Main()
+  {
+    int[] arrayDemo = new int[10];
+    int userChoice = 0;
+    while (userChoice != 999)
+    {
+      WriteLine("To view list from first to last, press 1");
+      WriteLine("To view list from last to first, press 2");
+      WriteLine("To view specific postion, press 3");
+      WriteLine("To exit when finished, press 4");
+      userChoice = Convert.ToInt32(ReadLine());
+      switch (userChoice)
+      {
+        case 1:
+          {
+            for(int x = 0; x >10;++x)
+            {
+              WriteLine()
+            }
+          }
+      }
+    }
+  }
 }
