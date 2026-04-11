ab72b9a7e819ce82af758009e9ade16a6489de61
Javier Flores <jflores0089@gmail.com>
2026-04-11 10:43:10 -0400

    Checkzips homework done

--- CheckZips/Program.cs
+++ CheckZips/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace CheckZips
+﻿using static System.Console;
+class CheckZips
 {
-    internal class Program
+     static void Main()
     {
-        static void Main(string[] args)
+        bool found = false;
+        
+        string [] zips = new string [10]{"12590","12601","56891","69001","10029","20456","34123","77734","09235","12345"};
+        
+        WriteLine("Please enter a valid zipcode");
+        
+        string userInput = ReadLine();
+        
+        for (int i = 0; i < 10; ++i)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            if (userInput == zips[i])
+            
+            {
+                found = true;
+            }   
+        }    
+            if (found)
+            {
+                WriteLine("Zipcode located.");
+            }
+            else
+            {
+                WriteLine("This Zipcode is incorrect!");
+            }
+        
+
     }
 }
+
