b1142f20a04b1634e428a1cf933419ce560d2b97
Javier Flores <Jflores0089@gmail.com>
2026-04-21 16:11:02 -0400

    Fahrenheit to Celsius

---
+++ Lab 3 #6/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+class F2C
+{
+    static void Main()
+    {
+        
+        WriteLine("Please enter the current temperature in Fahrenheit");
+        double userInput = double.Parse(ReadLine());
+        double celsius = (userInput - 32) * 5f / 9f;
+        string result = celsius.ToString("F1");
+        WriteLine("The temperature in Celsius is {0}",result);
+    }
+}
