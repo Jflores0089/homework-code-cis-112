4ee542d9a115995e47b10e472cd11993adba6ece
Javier Flores <Jflores0089@gmail.com>
2026-04-21 15:47:24 -0400

    Lab 3 question 5

---
+++ Lab 3(#5)/Program.cs
@@ -0,0 +1,24 @@
+﻿//Lab 3 invoice application
+
+using System.Text;
+using static System.Console;
+class InvoiceApp
+{
+    static void Main()
+    {
+        string apple = "Apple";
+        decimal cost = .50M;
+        int numPurchased = 6;
+        decimal subtotal = cost * numPurchased;
+        decimal taxRate = subtotal * 0.10M;
+        decimal total = subtotal + taxRate;
+        
+        WriteLine("Item:{0}", apple);
+        WriteLine("Quantity:{0}", numPurchased);
+        WriteLine("Unit Price:{0}", cost);
+        WriteLine("Subtotal:{0}", subtotal);
+        WriteLine("Tax:{0}", taxRate);
+        WriteLine("Total:{0}", total);
+        
+    }
+}
