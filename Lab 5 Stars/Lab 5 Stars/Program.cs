631716d6eab0444c8294c00463af7943f101fcc9
Javier Flores <jflores0089@gmail.com>
2026-04-22 23:31:56 -0400

    Lab 5 Stars question 6

---
+++ Lab 5 Stars/Lab 5 Stars/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+class Stars
+ 
+{
+static void Main()
+ 
+{
+    for (int x = 1; x <= 5; ++x)
+{
+    for (int y = 1; y <= 5 - x; ++y)
+{
+    Write(" ");
+}
+    for (int z = 1; z <= 2 * x - 1; ++z)
+        {
+        Write("*");
+        }
+        WriteLine();
+}
+}
+
+}
