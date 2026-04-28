8b3426f162760a4ca78656a8a02dedea4b094b95
Javier Flores <jflores0089@gmail.com>
2026-04-28 15:21:21 -0400

    Stars and Spaces done

---
+++ Lab4Question7/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+class StarsAndSpaces
+{
+    static void Main()
+    {
+        for (int x = 1; x <= 5; ++x)
+        {
+            if ( x == 1)
+            {
+                Write("    *");
+            }
+            else if (x != 1)
+            {
+               for (int i = 1; i <= 5 - x; ++i)
+                {
+                    Write(" ");
+                }
+                    Write("*");
+                for (int j = 1; j <= 2 * x - 3; ++j)
+                {
+                    Write(" ");   
+                }
+                    Write("*");
+            }
+            WriteLine();
+        }    
+    }
+}
