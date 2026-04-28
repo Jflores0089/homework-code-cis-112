4d84216c109d91acd90f1c5c9a5f0f845e038506
Javier Flores <jflores0089@gmail.com>
2026-04-28 17:19:55 -0400

    numbers game done

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+
+class NumbersGuessingGame
+{
+    static void Main()
+    {
+        int userInput = 0;
+        
+        Random rg = new Random();
+        
+        while (userInput != 999)
+        {
+            WriteLine("Welcome to the numbers guessing game!\n");
+            WriteLine("Please guess a number from 1 to 100.\n");
+            WriteLine("Otherwise type 999 to quit.\n");
+            
+            int rgNum = rg.Next(1,101);
+
+            int numTries = 0;
+
+            userInput = int.Parse(ReadLine());
+            
+            while (userInput != rgNum && userInput != 999)
+            {
+                numTries = ++numTries;
+                if (userInput != rgNum && userInput < rgNum) 
+                {
+                    WriteLine("Your guess is too low");
+                }
+                else if (userInput != rgNum && userInput > rgNum  )
+                {
+                    WriteLine("Your guess is too high");
+                }
+                WriteLine("Please type another guess");
+                userInput = int.Parse(ReadLine());
+            }
+            if (userInput == rgNum)
+            {
+            WriteLine("Congratulations! You guessed right!");
+            WriteLine("The number of guesses it took was {0}.",numTries);
+            }
+            else
+            {
+                if (userInput == 999)
+                {
+                    
+                    WriteLine("Thanks for playing!");
+                    WriteLine();
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
