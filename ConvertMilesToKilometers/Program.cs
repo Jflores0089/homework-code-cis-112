03eeb2218b03fcdb4f5f05546e10c107db8af34b
Javier Flores <jflores0089@gmail.com>
2026-04-28 22:47:46 -0400

    ConvertMilestoKilometers done

---
+++ ConvertMilesToKilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+class ConvertMilesToKilometers
+{
+    static double MtoK (double miles)
+    {
+        double kilos = miles * 1.60934;
+
+        return kilos;
+    }
+    static void Main()
+    {
+        WriteLine("Please enter the amount of miles");
+        double miles = double.Parse(ReadLine());
+         double kilos = MtoK(miles);
+         WriteLine("{0} miles is equal to {1} kilometers.", miles, kilos);
+    }
+}
