dd52e8b42b866f0d7dc528f6d34e91d93faf869c
Javier Flores <jflores0089@gmail.com>
2026-05-01 19:20:07 -0400

    Fine for over due books done

---
+++ FineForOverdueBooks/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+class FineForOverdueBooks
+{
+    
+
+   static double Fine(double books, double days)
+    {
+         double price = .10;
+         
+        if (days <= 7)
+        {
+          double total = days * books * price;
+
+           return total;
+        }
+        
+        else
+        {
+            double total = 0;
+            total = days * books * (2 * price);
+            
+            return total;
+        }
+    }
+    static void Main()
+    {
+        
+        WriteLine("How many books do you have overdue?");
+        double books = double.Parse(ReadLine());
+        WriteLine("How many days late?");
+        double days = double.Parse(ReadLine());
+        Fine(books, days);
+        WriteLine($"The total due is {Fine( books, days):C}");
+    }
+}
