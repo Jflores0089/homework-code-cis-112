466956a0c193d72658a9a7ae4f06f053b04d0fb3
Javier Flores <jflores0089@gmail.com>
2026-05-03 21:02:46 -0400

    digits in Integer done

---
+++ Lab6question2/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+class digitsInInteger
+{
+    static int Digits(int userInput)
+    {
+        int numTimesDivided = 0;
+        while (userInput > 0)
+        {
+            userInput /= 10;
+            ++numTimesDivided;
+        }
+        return numTimesDivided;
+    }
+    static void Main()
+    {
+        WriteLine("Please enter a numeric value.");
+        
+        int userInput = int.Parse(ReadLine());
+
+        Digits(userInput);
+        
+        WriteLine("The number of digits in your numeric value is {0}",Digits(userInput));
+    }
+}
