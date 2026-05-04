5a8d49d275d38a410064ae782241b71f4233d5d1
Javier Flores <jflores0089@gmail.com>
2026-05-04 00:14:23 -0400

    Lab 6 question 3 done

---
+++ Lab6question3/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+class sumOfIntegers
+{
+    static int SumOfInt(int userInput)
+    {
+        int sumInts = 0;
+        for ( int i = 1; i <= userInput; ++i)
+        {
+            sumInts = sumInts + i;
+        }
+        return sumInts;
+    }
+    static void Main()
+    {
+        int userInput = -1;
+        
+        while( userInput != 0)
+        {
+        
+            WriteLine("Please enter a number or type 0 to exit");
+        
+            userInput = int.Parse(ReadLine());
+        
+            if (userInput != 0)
+            {
+                SumOfInt(userInput);
+            
+                WriteLine("The sum of the integers up to the one you selected is {0}",SumOfInt(userInput));
+            }
+            else
+            {
+                WriteLine("Thanks for playing.");
+            }
+        }  
+    }
+}
+
