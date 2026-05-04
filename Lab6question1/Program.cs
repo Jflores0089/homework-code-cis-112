f093b0fc2bd9066aeb13cefbf117d4b3c6ffd3dd
Javier Flores <jflores0089@gmail.com>
2026-05-03 20:20:14 -0400

    Volume of box done

---
+++ Lab6question1/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+class VolumeOfBox
+{
+    static double Volume(double length, double width, double height)
+    {
+        return length * width * height;
+    }
+    static void Main()
+    {
+        double length = 0.0;
+        double width = 0.0;
+        double height = 0.0;
+
+        WriteLine("What is the length?");
+        length = double.Parse(ReadLine());
+        WriteLine("What is the width?");
+        width = double.Parse(ReadLine());
+        WriteLine("What is the height?");
+        height = double.Parse(ReadLine());
+
+        WriteLine("The volume is {0}.",Volume(length, width, height));
+
+    }
+}
