4983e15bbd09fd2d04d922361db4ae739f9f7cb1
Javier Flores <jflores0089@gmail.com>
2026-05-12 01:17:01 -0400

    Lab 6 question 4 Done

--- Lab6question4/Program.cs
+++ Lab6question4/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+class BabylonianMethod
+{
+   
+    private static double Square(double x)
+    {
+       double oldGuess = x;
+       
+       double newGuess = oldGuess / 2;
+       
+    while (oldGuess - newGuess > .00001)
+        {
+           double avgGuess = (newGuess + x / newGuess) / 2;
+           
+           oldGuess = newGuess;
+           
+           newGuess = avgGuess;
+        }
+        
+        return newGuess;
+
+    }
+    
+    static void Main()
+    {
+        WriteLine("Hello! Please enter a number!");
+        
+        double userInput = double.Parse(ReadLine()!);
+        
+        WriteLine("The square root of your number is approximately {0}", Square(userInput));
+
+    }
+}
